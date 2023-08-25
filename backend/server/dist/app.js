fb66999ee4b8d4173f6516f96739bcacd772381a
gabrielrguarini <gabrielrguarini@gmail.com>
2023-08-25 15:55:56 -0300

    vercel deploy

---
+++ backend/server/dist/app.js
@@ -0,0 +1,156 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const VerificaToken_1 = require("./auth/VerificaToken");
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const bcrypt = require('bcryptjs');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 3000;
+const CHAVE_SECRETA = process.env.CHAVE_SECRETA_JWT;
+const prisma = new client_1.PrismaClient();
+app.use(cors());
+app.use(express.json());
+app.post('/registro', async (req, res) => {
+    const { user, password } = req.body;
+    console.log(await postUser(user, password));
+});
+app.post("/auth", async (req, res) => {
+    const { id, user, password } = req.body;
+    const userInDb = await prisma.user.findFirst({
+        where: {
+            user
+        }
+    });
+    if (!userInDb) {
+        console.log("Usuário não existe");
+        return { message: "usuário não existe" };
+    }
+    const passwordIsValid = await bcrypt.compareSync(password, userInDb.password);
+    if (!passwordIsValid) {
+        return res.status(401).send({ auth: false, token: null });
+    }
+    const token = jwt.sign({ id }, CHAVE_SECRETA, { expiresIn: 86400 });
+    res.status(200).send({ auth: true, token });
+});
+app.get("/notas", VerificaToken_1.VerificaToken, async (req, res) => {
+    const notas = await getNotas().catch(async (error) => {
+        console.error(error);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
+    res.json(notas);
+});
+app.get("/notas/:notaId", VerificaToken_1.VerificaToken, async (req, res) => {
+    const nota = await getNota(req.params.notaId).catch(async (error) => {
+        console.error(error);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
+    res.json(nota);
+});
+app.get("/produtos", VerificaToken_1.VerificaToken, async (req, res) => {
+    const produtos = await getProdutos().catch(async (error) => {
+        console.error(error);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
+    res.json(produtos);
+});
+app.post("/", (req, res) => {
+    criaNota(req.body)
+        .then(async () => {
+        await prisma.$disconnect();
+    })
+        .catch(async (error) => {
+        console.error(error);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
+    res.send(req.body);
+});
+async function getNotas() {
+    try {
+        const notas = await prisma.nota.findMany();
+        return notas;
+    }
+    catch (error) {
+        console.error({ message: error });
+    }
+}
+async function getNota(id) {
+    try {
+        const nota = await prisma.nota.findUnique({
+            where: {
+                id
+            },
+            include: {
+                produtos: true
+            }
+        });
+        return nota;
+    }
+    catch (error) {
+        console.error({ message: error });
+    }
+}
+async function getProdutos() {
+    try {
+        const notas = await prisma.produto.findMany();
+        return notas;
+    }
+    catch (error) {
+        console.error({ message: error });
+    }
+}
+async function postUser(user, password) {
+    const userInDb = await prisma.user.findFirst({
+        where: {
+            user
+        }
+    });
+    if (userInDb) {
+        return { message: "Usuário já cadastrado" };
+    }
+    const hashedPassword = await bcrypt.hashSync(password, 8);
+    const registraUsuario = await prisma.user.create({
+        data: {
+            user,
+            password: hashedPassword
+        }
+    });
+    return registraUsuario;
+}
+async function criaNota({ afNumber, cidade, produtos }) {
+    const notaCriada = await prisma.nota.create({
+        data: {
+            afNumber: afNumber,
+            cidade,
+        }
+    });
+    for (const produto of produtos) { // Cria cada produto por vez e connecta ele com a nota.
+        try {
+            await prisma.produto.create({
+                data: {
+                    custo: produto.custo,
+                    nome: produto.nome,
+                    quantidade: produto.quantidade,
+                    status: produto.status,
+                    nota: {
+                        connect: {
+                            id: notaCriada.id
+                        }
+                    }
+                }
+            });
+        }
+        catch (error) {
+            console.error({ message: error });
+        }
+    }
+}
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`);
+});
---
+++ backend/server/src/auth/VerificaToken.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.VerificaToken = void 0;
+var jwt = require('jsonwebtoken');
+var CHAVE_SECRETA = process.env.CHAVE_SECRETA_JWT;
+function VerificaToken(req, res, next) {
+    var token = req.headers['x-access-token'];
+    if (!token)
+        return res.status(403).send({ auth: false, message: 'Nenhum token informado.' });
+    jwt.verify(token, CHAVE_SECRETA, function (err, decoded) {
+        if (err)
+            return res.status(500).send({ auth: false, message: 'Falha ao autenticar o token.' });
+        req.userId = decoded.id;
+        next();
+    });
+}
+exports.VerificaToken = VerificaToken;
